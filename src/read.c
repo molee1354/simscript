e12a6466fbc71a8c724b02562f294333509ff960
Mokin Lee <molee1354@gmail.com>
2023-10-17 01:54:19 +0900

    separate read file function

--- src/read.c
+++ src/read.c
@@ -1,26 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#include "common.h"
-#include "chunk.h"
-#include "debug.h"
-#include "vm.h"
-
-static void repl() {
-    puts("simscript v0.0.1");
-    char line[1024];
-    for (;;) {
-        printf("\n>>> ");
-
-        if (!fgets(line, sizeof(line), stdin)) {
-            printf("\n");
-            break;
-        }
-
-        interpret(line);
-    }
-}
+#include "reader.h"
 
 char* readFile(const char* path) {
     FILE* file = fopen(path, "rb");
@@ -53,30 +34,4 @@ char* readFile(const char* path) {
 
     fclose(file);
     return buffer;
-}
-
-static void runFile(const char* path) {
-    char* source = readFile(path);
-    InterpretResult result = interpret(source);
-    free(source);
-
-    if (result==INTERPRET_COMPILE_ERROR) exit(65);
-    if (result==INTERPRET_COMPILE_ERROR) exit(70);
-}
-
-int main(int argc, const char* argv[]) {
-    // init vm
-    initVM();
-
-    if (argc==1) {
-        repl();
-    } else if (argc==2) {
-        runFile(argv[1]);
-    } else {
-        fprintf(stderr, "Usage: hwin [path]\n");
-        exit(64);
-    }
-
-    freeVM();
-    return 0;
 }
---
+++ src/read.h
@@ -0,0 +1,6 @@
+#ifndef simscript_read_h
+#define simscript_read_h
+
+char* readFile(const char* path);
+
+#endif
