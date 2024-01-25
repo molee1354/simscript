7507c58c35adfb7fb256263bc79d0aa12127c352
Mokin Lee <molee1354@gmail.com>
2024-01-25 10:37:31 -0500

    Simscript v0.0.9rc1

--- src/main.c
+++ src/main.c
@@ -8,8 +8,8 @@
 #include "read.h"
 #include "vm.h"
 
-#define VERSION "0.0.8rc1"
-#define TIME    "Jan 02 2024, 20:36"
+#define VERSION "0.0.9rc1"
+#define TIME    "Jan 25 2024, 10:37"
 
 #ifdef _WIN32
 #define PLATFORM "Windows"
